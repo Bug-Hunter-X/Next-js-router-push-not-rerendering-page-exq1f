41417ecee3e5ad8fd826bbc02efcd24d57ac4265
BugHunter-X <askzenai@gmail.com>
2025-01-16 02:14:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,31 @@
+```javascript
+//pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Next.js Bug</h1>
+      <Link href='/about'>
+        <a>Go to About</a>
+      </Link>    </div>
+  );
+}
+```
+```javascript
+//pages/about.js
+import { useRouter } from 'next/router';
+
+export default function About(){
+    const router = useRouter();
+    const handleClick = () =>{
+        router.push('/');
+    };
+    return(
+        <div>
+            <h1>About Page</h1>
+            <button onClick={handleClick}>Go to Home</button>
+        </div>
+    );
+}
+```
